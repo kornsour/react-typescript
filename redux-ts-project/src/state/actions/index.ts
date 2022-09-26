5322fe500eb9d625c0bb73a95fc79433694e1203
kornsour <ajkaiserauer@gmail.com>
2022-09-26 16:30:16 -0400

    creating actions

---
+++ redux-ts-project/src/state/actions/index.ts
@@ -0,0 +1,21 @@
+import { ActionType } from '../action-types';
+
+interface SearchRepositoriesAction {
+  type: ActionType.SEARCH_REPOSITORIES;
+}
+
+interface SearchRepositoriesSuccessAction {
+  type: ActionType.SEARCH_REPOSITORIES_SUCCESS;
+  payload: string[];
+}
+
+interface SearchRepositoriesErrorAction {
+  type: ActionType.SEARCH_REPOSITORIES_ERROR;
+  payload: string;
+}
+
+// can be any of the three interfaces
+export type Action =
+  | SearchRepositoriesAction
+  | SearchRepositoriesSuccessAction
+  | SearchRepositoriesErrorAction;
